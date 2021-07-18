fb5abf301cfe86b8498636129cf1e2f070d36014
Jason Washburn <jason@shyftsolutions.io>
2021-07-18 08:28:15 -0500

    added user registration, login, and base templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -74,7 +74,7 @@ def login():
     return render_template("auth/login.html")
 
 
-@bp.before_app_request()
+@bp.before_app_request
 def load_logged_in_user():
     user_id = session.get("user_id")
 
