4c440964d3a0b83d214edcf9b548a768dbeb99f5
Jason Washburn <jason@shyftsolutions.io>
2021-07-18 08:00:46 -0500

    added user authorization logic

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,6 +30,11 @@ def create_app(test_config=None):
         return "Hello, World!"
 
     from . import db
+
     db.init_app(app)
+
+    from . import auth
+
+    app.register_blueprint(auth.bp)
 
     return app
