8b8271688e1e72283add6c5d7151fcd803cb6312
Jason Washburn <jason@shyftsolutions.io>
2021-07-19 05:31:36 -0500

    code formatting with black

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,20 @@ import pytest
 from flaskr import create_app
 from flaskr.db import get_db, init_db
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+    _data_sql = f.read().decode("utf8")
+
 
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
 
-    app=create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })
+    app = create_app(
+        {
+            "TESTING": True,
+            "DATABASE": db_path,
+        }
+    )
 
     with app.app_context():
         init_db()
